0e23b1c6a69b881595bf5821153ad1cb7f5274ca
Devojuks <devojukwu@gmail.com>
2022-09-12 02:27:08 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ *   main - Print "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if successful
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
