e483faa78f8137691e67ea5d59a22570f3fcc655
Devojuks <devojukwu@gmail.com>
2022-09-14 21:51:58 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,16 +10,12 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i <= 9; i++)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(c);
 		}
-		_puchar('\n');
+		_putchar)'\n');
 	}
 }
